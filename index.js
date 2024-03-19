22314e1598e7723de26e1e61519f9eaf7c616c46
Brian <brian.mwangi2@student.moringaschool.com>
2024-03-19 19:55:39 +0300

    creates file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+// Code your solution in this file
+const companyName=`Scuber`;
+
+console.log(companyName);
+
+//mostProfitableNeighborhood
+let mostProfitableNeighborhood=`Chelsea`;
+
+console.log(mostProfitableNeighborhood)
+
+//companyCeo
+let companyCeo=`Susan Smith`;
+
+console.log(companyCeo)
